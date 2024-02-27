9eb22190966f80cf058acf2efe1ae29e77574abe
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-27 14:42:58 +0100

    Mise à jour du composant Avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Image from "next/image";
+// import { ReactNode } from "react";
 
 type AvatarProps = {
   name: string;
   avatarUrl: string;
+  // children?: React.ReactNode;
 };
 
+// or use destructuring
+// const Avatar = ({ name, avatarUrl }: AvatarProps) => {
+// but below is more readable and shorter for the reader
 const Avatar = (props: AvatarProps) => {
   return (
     <div className="flex flex-col w-fit items-center gap-2">
       <Image
         className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
         src={props.avatarUrl}
-        alt="Bordered avatar"
+        alt={props.name}
         width={40}
         height={40}
         // priority
       />
       <p>{props.name}</p>
+      {/* <span>{props.children}</span> */}
+      {/* en fait c'est pas authorisé ça, mais ça fonctionne qund même xD */}
     </div>
   );
 };
