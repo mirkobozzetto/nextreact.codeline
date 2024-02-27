f84ae9a041eb950703d614413232100517431f61
Mirko Bozzetto <mirkouch@gmail.com>
2024-02-27 13:46:03 +0100

    Mise à jour de l'avatar et du nom de l'utilisateur

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,22 +3,21 @@ import Image from "next/image";
 
 type AvatarProps = {
   name: string;
+  avatarUrl: string;
 };
 
 const Avatar = (props: AvatarProps) => {
-  console.log(props);
-
   return (
     <div className="flex flex-col w-fit items-center gap-2">
       <Image
         className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-        src="https://gravatar.com/avatar/1f82b0492a0a938288c2d5b70534a1fb?s=400&d=robohash"
+        src={props.avatarUrl}
         alt="Bordered avatar"
         width={40}
         height={40}
         // priority
       />
-      <p>Jean</p>
+      <p>{props.name}</p>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Avatar from "./components/Avatar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Avatar />
+      <Avatar
+        name="Mirko"
+        avatarUrl="https://gravatar.com/avatar/1f82b0492a0a938288c2d5b70534a1fb?s=400&d=robohash"
+      />
     </main>
   );
 }
